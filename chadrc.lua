f54705edc21e724452a294d42c9c92c9e4c7e32e
Emokores256 <50874374+Emokores256@users.noreply.github.com>
2023-04-30 17:22:23 +0300

    Update chadrc.lua
    enable custom plugins

--- chadrc.lua
+++ chadrc.lua
@@ -1,5 +1,5 @@
 ---@type ChadrcConfig 
  local M = {}
  M.ui = {theme = 'doomchad'}
- M.plugins = "custom.plugins"
+ M.plugins = "custom.plugins" -- add this line to your chadrc.lua file to enable custom plugins
  return M
