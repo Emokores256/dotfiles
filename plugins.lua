f7783b8321af37e2bc5efcb3ac0a3f70293ae569
Emokores256 <50874374+Emokores256@users.noreply.github.com>
2023-04-30 17:21:08 +0300

    Add files via upload

---
+++ plugins.lua
@@ -0,0 +1,43 @@
+local plugins = {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults 
+        "vim",
+        "lua",
+
+        -- web dev 
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+        -- "vue", "svelte",
+
+       -- low level
+        "c",
+        "zig"
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+
+    dependencies = {
+      "jose-elias-alvarez/null-ls.nvim",
+      config = function()
+        require "custom.configs.null-ls"
+      end,
+    },
+
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  }
+}
+
+return plugins
