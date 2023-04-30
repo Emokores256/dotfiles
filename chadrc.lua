7114962ea147a3d4dc0ad8adc58eebfc47f0b211
Emokores256 <50874374+Emokores256@users.noreply.github.com>
2023-04-30 17:23:06 +0300

    Update chadrc.lua
    custom settings

--- chadrc.lua
+++ chadrc.lua
@@ -1,5 +1,5 @@
 ---@type ChadrcConfig 
  local M = {}
- M.ui = {theme = 'doomchad'}
+ M.ui = {theme = 'doomchad'} -- for your custom theme from NvChad themes
  M.plugins = "custom.plugins" -- add this line to your chadrc.lua file to enable custom plugins
  return M
