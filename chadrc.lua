d02df30c073cd657acfe7cb0c0acbaa30163e0dc
Emokores256 <50874374+Emokores256@users.noreply.github.com>
2023-05-05 22:52:17 +0300

    Update chadrc.lua - added mappings

--- chadrc.lua
+++ chadrc.lua
@@ -2,4 +2,5 @@
  local M = {}
  M.ui = {theme = 'doomchad'} -- for your custom theme from NvChad themes
  M.plugins = "custom.plugins" -- add this line to your chadrc.lua file to enable custom plugins
+ M.mappings = require "custom.mappings"
  return M
